e433ac6f2a7ba9e6f8ef70f2c43cf363f897d2b3
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-28 10:48:22 +0800

    cc

--- vis/vis.py
+++ vis/vis.py
@@ -40,12 +40,12 @@ from config import config
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Detectron2 demo for builtin configs")
-    parser.add_argument(
-        "--config-file",
-        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
-        metavar="FILE",
-        help="path to config file",
-    )
+    # parser.add_argument(
+    #     "--config-file",
+    #     default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+    #     metavar="FILE",
+    #     help="path to config file",
+    # )
     parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
     parser.add_argument("--video-input", help="Path to video file.")
     parser.add_argument(
